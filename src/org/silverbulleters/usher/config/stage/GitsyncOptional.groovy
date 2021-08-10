1d2ac8ba6dae947a07278eb331d781dc37cfc49f
Tymko Oleg <olegtymko@yandex.ru>
2021-08-10 22:57:58 +0700

    fix: использование Repo в PrepareBase

--- src/org/silverbulleters/usher/config/stage/GitsyncOptional.groovy
+++ src/org/silverbulleters/usher/config/stage/GitsyncOptional.groovy
@@ -7,13 +7,11 @@
 package org.silverbulleters.usher.config.stage
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import org.silverbulleters.usher.config.additional.Repo
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 class GitsyncOptional extends BaseOptional {
   static final EMPTY = new GitsyncOptional()
 
-  Repo repo = Repo.EMPTY
   String configPath = "./tools/JSON/gitsync_conf.JSON"
 
   GitsyncOptional() {
--- vars/prepareBase.groovy
+++ vars/prepareBase.groovy
@@ -7,6 +7,7 @@
 import groovy.transform.Field
 import org.silverbulleters.usher.UsherConstant
 import org.silverbulleters.usher.config.PipelineConfiguration
+import org.silverbulleters.usher.config.additional.Repo
 import org.silverbulleters.usher.config.stage.PrepareBaseOptional
 
 @Field
