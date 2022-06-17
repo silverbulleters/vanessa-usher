6d78715f8ff70ab5c845043160b2bc5b7fd89dd4
Огородов Илья <iogorodov@silverbulleters.org>
2022-06-17 21:34:34 +0400

    fix/поправил запуск тест-клиента в дымовых тестах

--- vars/credentialHelper.groovy
+++ vars/credentialHelper.groovy
@@ -51,10 +51,10 @@ String getCustomAuth(String userName, String passName) {
  */
 String getTestClientWithAuth() {
   def baseValue = '%s:%s:1538'
-  login = "${USERNAME}"
+  login = "${DBUSERNAME}"
   pass = ""
   try {
-    pass = "${PASSWORD}"
+    pass = "${DBPASSWORD}"
   } catch (ignored) {
   }
   def credentialTestClient = String.format(baseValue, login, pass)
